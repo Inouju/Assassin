015ec44847b301a6d9be674f2bdd7b2401a70f8c
Justin Inouye <inouju@gmail.com>
2016-03-03 10:39:32 -0800

    Attach test adapter to leaderboard

--- app/src/main/java/edu/uw/jyinouye/assassin/LeaderboardActivity.java
+++ app/src/main/java/edu/uw/jyinouye/assassin/LeaderboardActivity.java
@@ -2,12 +2,26 @@ package edu.uw.jyinouye.assassin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    List<String> leaders = Arrays.asList("1", "Test User", "2", "3");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        ListView leaderList = (ListView)findViewById(R.id.leaderboard_listview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.leaderboard_list_item, leaders);
+
+        leaderList.setAdapter(adapter);
     }
 }
