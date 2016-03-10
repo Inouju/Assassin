bcfbcce2b1b49fef9301963ee516b79cef81457f
colec <colechamberlin@gmail.com>
2016-03-10 00:54:11 -0800

    Commit any changes

--- app/src/main/java/edu/uw/jyinouye/assassin/MainActivity.java
+++ app/src/main/java/edu/uw/jyinouye/assassin/MainActivity.java
@@ -498,7 +498,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     databasePlayer.setTargetuid(databasePlayer.getTargetuid());
                     players.put(dataSnapshot.getKey(), databasePlayer);
                 }
-                if(databasePlayer.getUid().equals(player.getUid())) {
+                if(databasePlayer != null && databasePlayer.getUid().equals(player.getUid())) {
                     player.setTargetuid(databasePlayer.getTargetuid());
                     // if player is dead
                     if(databasePlayer.getIsDead()) {
